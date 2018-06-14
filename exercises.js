201f8574cd325c8aa4a03216a5bf0e29edea5bff
BRIAN   AND DONNA WONG <loganwong@student.midpac.edu>
2018-06-13 19:45:35 -1000

    Completed exercise #2

--- exercises.js
+++ exercises.js
@@ -34,8 +34,11 @@ while (i < 10) {
 ...
 ...
 "Now serving 11"*/
-
-
+var i = 0;
+while (i < 11) {
+	i ++;
+	console.log("Now serving " + i);
+}
 
 /*3. Use a while loop to loop through the tvShows array and print out the following:
 
